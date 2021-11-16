137d866c873f413fa35ca2b908c58fafcbca2a34
Daniel Herszenhut <dhersz@gmail.com>
2021-11-16 15:12:12 -0300

    atualizacao de funcao pra corrigir velocidades

--- R/correcao_velocidades_gtfs.R
+++ R/correcao_velocidades_gtfs.R
@@ -1,12 +1,20 @@
-corrigir_velocidades_gtfs <- function() {
+corrigir_velocidades_gtfs <- function(log = TRUE) {
 
   anos <- 2017:2020
   cidades <- munis_list$munis_df$abrev_muni
 
+  if (log) {
+    caminho_log <- "../../data/gtfs/log.txt"
+    con <- file(caminho_log)
+    on.exit(close(con), add = TRUE)
+    sink(caminho_log)
+    on.exit(sink(), add = TRUE)
+  }
+
   # pra cada ano e cidade, checa quais possuem arquivos gtfs
   for (ano in anos) {
     for (cidade in cidades) {
-      message(cidade, " - ", ano)
+      cat(cidade, "-", ano, "\n")
 
       dir_arquivos <- file.path("../../otp/graphs", ano, cidade)
       arquivos <- list.files(dir_arquivos)
@@ -15,25 +23,33 @@ corrigir_velocidades_gtfs <- function() {
 
       if (!identical(arquivos_gtfs, character(0))) {
 
-        dir_resultado <- file.path("../../data/gtfs", cidade, ano)
-        if (!dir.exists(dir_resultado)) {
-          dir.create(dir_resultado, recursive = TRUE)
-        }
+        withCallingHandlers(
+          {
+            dir_resultado <- file.path("../../data/gtfs", cidade, ano)
+            if (!dir.exists(dir_resultado)) {
+              dir.create(dir_resultado, recursive = TRUE)
+            }
 
-        # pra cada arquivo gtfs encontrado, corrige a velocidade e salva na nova
-        # pasta com nome atualizado
-        for (antigo_gtfs in arquivos_gtfs) {
-          caminho_antigo_gtfs <- file.path(dir_arquivos, antigo_gtfs)
+            # pra cada arquivo gtfs encontrado, corrige a velocidade e salva na nova
+            # pasta com nome atualizado
+            for (antigo_gtfs in arquivos_gtfs) {
+              cat("  *", antigo_gtfs, "\n")
+              caminho_antigo_gtfs <- file.path(dir_arquivos, antigo_gtfs)
 
-          novo_gtfs <- corrigir_velocidades(caminho_antigo_gtfs)
-          caminho_novo_gtfs <- file.path(
-            dir_resultado,
-            sub("\\.zip", "_updated_speed.zip", antigo_gtfs)
-          )
+              novo_gtfs <- corrigir_velocidades(caminho_antigo_gtfs, log)
+              caminho_novo_gtfs <- file.path(
+                dir_resultado,
+                sub("\\.zip", "_updated_speed.zip", antigo_gtfs)
+              )
 
-          gtfstools::write_gtfs(novo_gtfs, caminho_novo_gtfs)
-        }
+              gtfstools::write_gtfs(novo_gtfs, caminho_novo_gtfs)
+            }
+          },
+          warning = function(cnd) if (log) print(cnd$message)
+        )
       }
+
+      if (log) cat("\n")
     }
   }
 
@@ -41,11 +57,14 @@ corrigir_velocidades_gtfs <- function() {
 
 
 
-corrigir_velocidades <- function(caminho) {
+corrigir_velocidades <- function(caminho, log) {
   gtfs <- gtfstools::read_gtfs(caminho)
 
-  if (gtfsio::check_files_exist(gtfs, "shapes")) {
+  if (gtfsio::check_file_exists(gtfs, "shapes")) {
     viagens <- gtfstools::get_trip_speed(gtfs)
+
+    if (log) print(summary(viagens))
+
     viagens_problematicas <- viagens[speed > 80 | speed < 2]$trip_id
 
     # talvez adicionar um na.rm = TRUE aqui depois
@@ -53,7 +72,7 @@ corrigir_velocidades <- function(caminho) {
       viagens[speed <= 80 & speed >= 2,]$speed
     )
 
-    novo_gtfs <- gtfstools::set_trip_speed(
+    gtfs <- gtfstools::set_trip_speed(
       gtfs,
       viagens_problematicas,
       speed = velocidade_media
