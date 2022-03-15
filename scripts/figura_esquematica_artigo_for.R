9d000962a0a3f84c45e9d8c7f42d608f58295280
Daniel Herszenhut <dhersz@gmail.com>
2022-03-15 17:07:57 -0300

    finaliza figura do artigo de fort.

--- scripts/figura_esquematica_artigo_for.R
+++ scripts/figura_esquematica_artigo_for.R
@@ -2,6 +2,7 @@ library(data.table)
 library(ggplot2)
 library(cowplot)
 library(ggtext)
+library(latex2exp)
 
 set.seed(0)
 
@@ -13,16 +14,185 @@ tema <- theme_minimal() +
   theme(
     panel.grid = element_blank(),
     axis.line = element_line(arrow = arrow(type = "closed", length = unit(5, "points"))),
-    axis.title.x = element_text(hjust = 1, margin = margin(t = -8)),
-    axis.text.x = element_markdown(margin = margin())
+    axis.title.x = element_text(hjust = 1, margin = margin(t = -10)),
+    axis.text.x = element_markdown(size = 7.5, margin = margin())
   )
+
+# grafico convencional ----------------------------------------------------
+
+pontos <- c(
+  "t<sub>1</sub>" = 15,
+  "t<sub>2</sub>" = 30,
+  "t<sub>3</sub>" = 45,
+  "..." = 70,
+  "t<sub>n</sub>" = 95
+)
+access_pontos <- dist[tt %in% pontos]$cum_access
+names(access_pontos) <- c(
+  "a<sub>1</sub>",
+  "a<sub>2</sub>",
+  "a<sub>3</sub>",
+  "...",
+  "a<sub>n</sub>"
+)
+
+segmentos_verticais <- data.table(
+  x = pontos,
+  xend = pontos,
+  y = 0,
+  yend = access_pontos
+)
+segmentos_verticais <- segmentos_verticais[-4]
+
+segmentos_horizontais <- data.table(
+  x = 0,
+  xend = pontos,
+  y = access_pontos,
+  yend = access_pontos
+)
+segmentos_horizontais <- segmentos_horizontais[-4]
 
 p1 <- ggplot(dist) +
   geom_line(aes(tt, cum_access), color = "gray40") +
-  scale_x_continuous(name = "T") +
-  scale_y_continuous(name = "A")
+  geom_segment(
+    data = segmentos_verticais,
+    aes(x = x, xend = xend, y = y, yend = yend),
+    linetype = "dotted",
+    alpha = 0.5
+  ) +
+  geom_segment(
+    data = segmentos_horizontais,
+    aes(x = x, xend = xend, y = y, yend = yend),
+    linetype = "dotted",
+    alpha = 0.5
+  ) +
+  scale_x_continuous(
+    name = "T",
+    breaks = pontos,
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+  scale_y_continuous(
+    name = "A",
+    breaks = access_pontos,
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+  tema +
+  theme(
+    axis.text.y = element_markdown(size = 7.5),
+    axis.title.y = element_text(hjust = 1, angle = 0, margin = margin(r = -10)),
+    plot.margin = margin(t = 5, r = 5, b = 5, l = 18)
+  )
 
-parrow <- ggplot() +
+# grafico com janelas -----------------------------------------------------
+
+tempos_janelas <- c(
+  "t<sub>w1<sub>min</sub></sub>" = 15,
+  "t<sub>w1<sub>max</sub></sub>" = 30,
+  "t<sub>w2<sub>min</sub></sub>" = 45,
+  "t<sub>w2<sub>max</sub></sub>" = 60,
+  "t<sub>wn<sub>min</sub></sub>" = 80,
+  "t<sub>wn<sub>max</sub></sub>" = 105
+)
+access_janelas <- dist[tt %in% tempos_janelas]$cum_access
+names(access_janelas) <- rep("", 6)
+
+janelas_verticais <- data.table(
+  x = tempos_janelas,
+  xend = tempos_janelas,
+  y = 0,
+  yend = access_janelas
+)
+janelas_horizontais <- data.table(
+  x = 0,
+  xend = tempos_janelas,
+  y = access_janelas,
+  yend = access_janelas
+)
+nomes_janelas <- data.table(
+  x = c(tempos_janelas[c(1, 3, 5)] + 1, 68),
+  y = 1,
+  label = c("w1", "w2", "wn", "...")
+)
+
+x_colchetes <- -3
+colchetes <- data.table(
+  x = x_colchetes,
+  xend = x_colchetes,
+  y = access_janelas[c(1, 3, 5)],
+  yend = access_janelas[c(2, 4, 6)]
+)
+barrinhas_colchetes <- data.table(
+  x = x_colchetes - 1,
+  xend = x_colchetes + 1,
+  y = access_janelas,
+  yend = access_janelas
+)
+breaks_y = c(
+  (access_janelas[1] + access_janelas[2]) / 2,
+  (access_janelas[3] + access_janelas[4]) / 2,
+  (access_janelas[5] + access_janelas[6]) / 2
+)
+labels_y = c(
+  TeX("\\bar{$a_{w1}$}"),
+  TeX("\\bar{$a_{w2}$}"),
+  TeX("\\bar{$a_{wn}$}")
+)
+
+p2 <- ggplot(dist) +
+  geom_line(aes(tt, cum_access), color = "gray40") +
+  geom_segment(
+    data = janelas_verticais,
+    aes(x = x, xend = xend, y = y, yend = yend),
+    linetype = "longdash",
+    color = rep(c("firebrick3", "dodgerblue3", "goldenrod3"), each = 2)
+  ) +
+  geom_segment(
+    data = janelas_horizontais,
+    aes(x = x, xend = xend, y = y, yend = yend),
+    linetype = "dotted",
+    color = rep(c("firebrick3", "dodgerblue3", "goldenrod3"), each = 2),
+    alpha = 0.5
+  ) +
+  geom_text(
+    data = nomes_janelas,
+    aes(x = x, y = y, label = label),
+    hjust = 0,
+    vjust = 0,
+    color = c("firebrick3", "dodgerblue3", "goldenrod3", "gray20"),
+    size = 2.5
+  ) +
+  geom_segment(
+    data = colchetes,
+    aes(x = x, xend = xend, y = y, yend = yend),
+    linetype = "solid",
+    color = "gray30"
+  ) +
+  geom_segment(
+    data = barrinhas_colchetes,
+    aes(x = x, xend = xend, y = y, yend = yend),
+    linetype = "solid",
+    color = "gray30"
+  ) +
+  scale_x_continuous(
+    name = "T",
+    breaks = tempos_janelas,
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+  scale_y_continuous(
+    name = "A",
+    breaks = breaks_y,
+    labels = labels_y,
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+  coord_cartesian(xlim = c(0, 120), clip = "off") +
+  tema +
+  theme(
+    axis.text.y = element_text(size = 7.5, margin = margin(r = 5)),
+    axis.title.y = element_text(hjust = 1, angle = 0, margin = margin(r = -15)),
+    plot.margin = margin(t = 5, r = 5, b = 5, l = 18)
+  )
+
+seta <- ggplot() +
   geom_segment(
     aes(x = 0, xend = 1, y = 0, yend = 0),
     arrow = arrow(length = unit(10, "points"))
@@ -31,202 +201,14 @@ parrow <- ggplot() +
   theme(
     axis.title = element_blank(),
     axis.text = element_blank(),
-    panel.grid = element_blank(),
-    plot.margin = margin(r = -15)
+    panel.grid = element_blank()
   )
-
-janela1 <- c("t<sub>w1,min</sub>" = 15, "t<sub>w1,max</sub>" = 45)
-janela2 <- c("t<sub>w2,min</sub>" = 75, "t<sub>w1,max</sub>" = 105)
-
-access1 <- c(
-  "a<sub>w1,min</sub>" = dist[tt == janela1[1]]$cum_access,
-  "a<sub>w1,max</sub>" = dist[tt == janela1[2]]$cum_access
-)
-access2 <- c(
-  "a<sub>w2,min</sub>" = dist[tt == janela2[1]]$cum_access,
-  "a<sub>w2,max</sub>" = dist[tt == janela2[2]]$cum_access
-)
-
-p1_com_janelas <- ggplot(dist) +
-  geom_line(aes(tt, cum_access), color = "gray40") +
-  annotate(
-    geom = "segment",
-    x = janela1[1],
-    xend = janela1[1],
-    y = 0,
-    yend = 40,
-    linetype = "longdash",
-    color = "firebrick3"
-  ) +
-  annotate(
-    geom = "text",
-    x = janela1[1] + 2,
-    y = 1,
-    color = "firebrick3",
-    label = "w1",
-    size = 3,
-    hjust = 0,
-    vjust = 0
-  ) +
-  annotate(
-    geom = "segment",
-    x = janela1[2],
-    xend = janela1[2],
-    y = 0,
-    yend = 40,
-    linetype = "longdash",
-    color = "firebrick3"
-  ) +
-  annotate(
-    geom = "segment",
-    x = janela2[1],
-    xend = janela2[1],
-    y = 0,
-    yend = 80,
-    linetype = "longdash",
-    color = "dodgerblue3"
-  ) +
-  annotate(
-    geom = "text",
-    x = janela2[1] + 2,
-    y = 1,
-    color = "dodgerblue3",
-    label = "w2",
-    size = 3,
-    hjust = 0,
-    vjust = 0
-  ) +
-  annotate(
-    geom = "segment",
-    x = janela2[2],
-    xend = janela2[2],
-    y = 0,
-    yend = 80,
-    linetype = "longdash",
-    color = "dodgerblue3"
-  )
-
-p2 <- p1_com_janelas +
-  geom_line(
-    data = dist[tt >= janela1[1] & tt <= janela1[2]],
-    mapping = aes(tt, cum_access),
-    color = "firebrick3"
-  ) +
-  geom_line(
-    data = dist[tt >= janela2[1] & tt <= janela2[2]],
-    mapping = aes(tt, cum_access),
-    color = "dodgerblue3"
-  ) +
-  annotate(
-    geom = "segment",
-    x = 0,
-    xend = janela1[1],
-    y = dist[tt == janela1[1]]$cum_access,
-    yend = dist[tt == janela1[1]]$cum_access,
-    linetype = "dotted",
-    color = "firebrick3",
-    alpha = 0.5
-  ) +
-  annotate(
-    geom = "segment",
-    x = 0,
-    xend = janela1[2],
-    y = dist[tt == janela1[2]]$cum_access,
-    yend = dist[tt == janela1[2]]$cum_access,
-    linetype = "dotted",
-    color = "firebrick3",
-    alpha = 0.5
-  ) +
-  annotate(
-    geom = "segment",
-    x = 0,
-    xend = janela2[1],
-    y = dist[tt == janela2[1]]$cum_access,
-    yend = dist[tt == janela2[1]]$cum_access,
-    linetype = "dotted",
-    color = "dodgerblue3",
-    alpha = 0.5
-  ) +
-  annotate(
-    geom = "segment",
-    x = 0,
-    xend = janela2[2],
-    y = dist[tt == janela2[2]]$cum_access,
-    yend = dist[tt == janela2[2]]$cum_access,
-    linetype = "dotted",
-    color = "dodgerblue3",
-    alpha = 0.5
-  ) +
-  scale_x_continuous(
-    name = "T",
-    breaks = c(janela1, janela2),
-    expand = expansion(0, 0.05)
-  ) +
-  scale_y_continuous(
-    name = "A",
-    breaks = c(access1, access2),
-    expand = expansion(0, 0.05)
-  ) +
-  tema +
-  theme(
-    axis.text.y = element_markdown(),
-    axis.title.y = element_text(hjust = 1, angle = 0, margin = margin(r = -28)),
-    plot.margin = margin(t = 5, r = 5, b = 5, l = 28)
-  )
-
-avg_access1 <- mean(dist[tt >= janela1[1] & tt <= janela1[2]]$cum_access)
-avg_access2 <- mean(dist[tt >= janela2[1] & tt <= janela2[2]]$cum_access)
-
-p3 <- p1_com_janelas +
-  geom_segment(
-    aes(x = janela1[1], xend = janela1[2], y = avg_access1, yend = avg_access1),
-    color = "firebrick3"
-  ) +
-  geom_segment(
-    aes(x = janela2[1], xend = janela2[2], y = avg_access2, yend = avg_access2),
-    color = "dodgerblue3"
-  ) +
-  scale_x_continuous(
-    name = "T", breaks = c(janela1, janela2),
-    expand = expansion(0, 0.05)
-  ) +
-  scale_y_continuous(
-    name = "A",
-    breaks = c(avg_access1, avg_access2),
-    labels = c(TeX("\\bar{$a_{w1}$}"), TeX("\\bar{$a_{w2}$}")),
-    expand = expansion(0, 0.05)
-  ) +
-  annotate(
-    geom = "segment",
-    x = 0,
-    xend = janela1[1],
-    y = avg_access1,
-    yend = avg_access1,
-    linetype = "dotted",
-    color = "firebrick3",
-    alpha = 0.5
-  ) +
-  annotate(
-    geom = "segment",
-    x = 0,
-    xend = janela2[1],
-    y = avg_access2,
-    yend = avg_access2,
-    linetype = "dotted",
-    color = "dodgerblue3",
-    alpha = 0.5
-  ) +
-  tema +
-  theme(
-    axis.title.y = element_text(hjust = 1, angle = 0, margin = margin(r = -12)),
-    plot.margin = margin(t = 5, r = 5, b = 5, l = 28)
-  ) +
-  expand_limits(x = 0, y = 0)
 
 pf <- plot_grid(
-  p2, parrow, p3,
+  p1, p2,
   nrow = 1,
-  rel_widths = c(1, 0.15, 1)
+  align = "h",
+  labels = c("a", "b")
 )
 
 ggsave(
@@ -236,4 +218,3 @@ ggsave(
   height = 5,
   units = "cm"
 )
-
